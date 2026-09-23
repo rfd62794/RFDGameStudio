b58fac3b45f68cacb09db901444b2f2cf7e301ef
rfd62794 <cheater2478@gmail.com>
2026-09-22 21:44:49 -0400

    foundation: bundle games/*/glossary.yaml, getGlossary, JSON-schema mirror

---
+++ ts/src/foundation/glossary/index.ts
@@ -0,0 +1,5 @@
+export * from './schema';
+export { parseGlossary } from './validate';
+export { resolveBind } from './bind';
+export { fillTemplate, placeholders } from './template';
+export { getGlossary, glossaryGameIds } from './load';
